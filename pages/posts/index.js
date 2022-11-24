f49c504a8417620c77ea5198d391348c59bcb973
Arif6474 <arifulislam64743@gmail.com>
2022-11-24 16:13:58 +0600

    dynamic post

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 function index({data}) {
-    console.log(data);
+    // console.log(data);
     return (
         <div>
            <h1>This is a post page</h1>
            {
            data.map(d => {
             return <div d={d} key={d.id}>
+            <Link href={`/posts/${d.id}`}>
                 <h2>{d.title}</h2>
+                </Link>
                 <h4>{d.userId}</h4>
                 <p>{d.body}</p>
                 <hr></hr>
@@ -17,7 +21,7 @@ function index({data}) {
     );
 }
 
-export async function getServerSideProps(ctx){
+export async function getStaticProps(ctx){
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
 
