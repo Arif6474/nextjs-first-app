7c64524ad9934e9a505723710b7d4b900ebf56c4
Arif6474 <arifulislam64743@gmail.com>
2022-11-24 13:52:50 +0600

    change route progtammatically

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -5,9 +5,13 @@ function BlogDetails() {
     const router = useRouter();
     const blogId = router.query.blogId
     console.log(blogId);
+    const goToAbout = () => {
+        router.replace('/about');
+    }
     return (
         <div>
     <h1>Blog details page {blogId}</h1>
+    <button onClick={goToAbout}>submit</button>
         </div>
     );
 }
